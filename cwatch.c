6c83c4d5778fb3f72f9bc7cfaae3828a7b0da730
k3nny <k3nny.wx@mailfence.com>
2023-04-18 22:12:47 +0800

    added foundation for pattern matching

--- cwatch.c
+++ cwatch.c
@@ -1,19 +1,23 @@
 /** includes **/
+#include "util.h"
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 
 static int e_cur;
 static int e_tot;
-int e_rem;
+
+char c;
 
 static char logdir[128];
 
 int e_logdir() {
     FILE *fpipe;
     char buf[128];
+    char cmd[] = "portageq envvar EMERGE_LOG_DIR";
     
-    if (0 == (fpipe = (FILE*)popen("portageq envvar EMERGE_LOG_DIR", "r")))
+    if (0 == (fpipe = (FILE*)popen(cmd, "r")))
     {
         perror ("popen() failed.");
         exit (EXIT_FAILURE);
@@ -22,19 +26,23 @@ int e_logdir() {
     fread(&buf, sizeof buf, 128, fpipe);
     
     /*
-    if (  logdir empty string  )
-        return 0; // logdir = /var/log/portage/
+    if (  &buf empty string  )
+        logdir = /var/log/portage/
     else
-        return 0; // logdir = &buf
+        logdir = &buf
     */
     pclose(fpipe);
 
     return EXIT_SUCCESS;
 }
 
+void read_char() {
+    if ((c = getchar()) == EOF) exit(EXIT_FAILURE);
+}
+
 static void log_parse() {
     // fstream reverse read
-    // pattern match ">>> energe"
+    // pattern match ">>> emerge"
     // awk 2 fields: [1] of [3]
     //
     // parse popen() commands >>> static var e_cur / e_tot
@@ -43,19 +51,27 @@ static void log_parse() {
     // cat /var/log/portage/emerge.log  | grep -w ">>> emerge" | awk '{print $6}'
 }
 
-int calc_rem() {
-    e_rem = e_tot - e_cur + 1;
-}
-
-
-
 int main() {
-    e_logdir(); 
-    
-    log_parse();
-    calc_rem();
-    
+    int i, match = 0;
+    char str[] = "insects";
 
+    read_char();
+    for (i=0; str[i] != '\0'; i++) {
+        if (c == str[i]) {
+            match = 1;
+            putchar(c);
+            read_char();
+        }
+        else if (match) {
+            match = 0;
+            i = -1;
+        }
+        else {
+            i= -1;
+            putchar(c);
+            read_char();
+        }
+    }
 
 
     return 0;
---
+++ util.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LEN(x) (sizeof (x) / sizeof *(x))
+
+
