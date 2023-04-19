613febd6cf762b8a064eaf071c55806217481d1a
k3nny <k3nny.wx@mailfence.com>
2023-04-19 10:01:40 +0800

    arg parsing, partial unit tests

--- cwatch.c
+++ cwatch.c
@@ -1,8 +1,14 @@
+// This program is licensed under the GNU Public License (GPL).
+// All indentation is performed according to GNU indentation standards.
+// This program is to be used AS IS, and does NOT include any WARRANTY,
+// even for MERCHANTABILITY, or FITNESS FOR A PARTICULAR PURPOSE.
+
+
 /**** TO DO ****/
 
 /* Move repeatedly used functions / basic definitions to lib.h
-Populate functions
-Create first public release */
+   Populate functions
+   Create first public release */
 
 
 /** includes **/
@@ -14,29 +20,46 @@ Create first public release */
 #include <string.h>
 #include <argp.h>
 
-static int e_cur;
-static int e_tot;
-static int usr_dir;
+static void prev_time ();
+static void avg_time ();
+static void pkg_hist ();
+static void global_hist ();
 
-char c;
+static char c;
 static char logdir[128];
+static char std_dir[] = "/var/log/portage";
+
+// static int e_cur;
+// static int e_tot;
+// static int usr_dir;
 
 static int
 parse_opt (int key, char *arg, struct argp_state *state)
 {
   switch (key)
-  {
-  case 't': printf(".\n")
     {
-      prevtime()
+    case 't':
+      {
+	prev_time ();
+	break;
+      };
+    case 'T':
+      {
+	avg_time ();
+	break;
+      };
+    case 'p':
+      {
+	pkg_hist ();
+	break;
+      };
+    case 'g':
+      {
+	global_hist ();
+	break;
+      };
     }
-  case 'T': printf(".\n"); break;
-  case 'p': printf(".\n"); break;
-  case 'g': printf(".\n"); break;
-  }
   return 0;
-  // add flags for functions
-  // v,g,h,s,t,T
 }
 
 int
@@ -58,9 +81,10 @@ get_logdir ()
 
   if (buf[0] != '\0')
     strcpy (logdir, buf);
+    // possible switch to strncpy()?
   else
-    exit (EXIT_FAILURE);
-  // temporary, option will be enabled soon
+    strcpy (logdir, std_dir);
+    // possible switch to strncpy()?
 
   puts (logdir);
 
@@ -74,7 +98,6 @@ read_char ()
   if ((c = getchar ()) == EOF)
     exit (EXIT_FAILURE);
 }
-*/
 
 static void
 log_parse ()
@@ -88,33 +111,37 @@ log_parse ()
   // cat /var/log/portage/emerge.log  | grep -w ">>> emerge" | awk '{print $4}'
   // cat /var/log/portage/emerge.log  | grep -w ">>> emerge" | awk '{print $6}'
 }
+*/
 
-static void prev_time () {
-  //
+prev_time ()
+{
+  printf ("prev_time [ok]\n");
 }
 
-static void avg_time () {
-  //
+avg_time ()
+{
+  printf ("avg_time [ok]\n");
 }
 
-static void pkg_hist () {
-  //
+pkg_hist ()
+{
+  printf ("pkg_hist [ok]\n");
 }
 
-static void global_hist () {
-  //
+global_hist ()
+{
+  printf ("global_hist [ok]\n");
 }
 
 int
 main (int argc, char **argv)
 {
-  struct argp_option options[] =
-  {
-    { "prevtime", 't', "<pkg>", 0, "Print the last build time for <pkg>" },
-    { "avgtime", 'T', "<pkg>", 0, "Calculate the avg build time for <pkg>" },
-    { "pkg-history", 'p', "<pkg>", 0, "Fetch emerge history for <pkg>" },
-    { "global-history", 'g', 0, 0, "Fetch global emerge history" },
-    { 0 }
+  struct argp_option options[] = {
+    {"prevtime", 't', "<pkg>", 0, "Print the last build time for <pkg>"},
+    {"avgtime", 'T', "<pkg>", 0, "Calculate the avg build time for <pkg>"},
+    {"pkg-history", 'p', "<pkg>", 0, "Fetch emerge history for <pkg>"},
+    {"global-history", 'g', 0, 0, "Fetch global emerge history"},
+    {0}
   };
   struct argp argp = { options, parse_opt, 0, 0 };
   return argp_parse (&argp, argc, argv, 0, 0, 0);
